c030986c2a7d964d1e5e71efbef623987502ddba
mitko <mitko_2045@abv.bg>
2022-04-12 20:17:28 +0300

    task 2 - introduction to switch

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
 
+        Scanner inputScanner   = new Scanner(System.in);
+        final int OBJECT_WALL  = 1;
+        final int OBJECT_CHAIR = 2;
+        final int NO_OBJECT    = 3;
+
+
+        while(true) {
+            System.out.println("Какво има на пътя ми ? ");
+            System.out.println("1. Стена ");
+            System.out.println("2. Стол ");
+            System.out.println("3. Чисто е ");
+
+            int objectId = inputScanner.nextInt();
+
+            switch (objectId) {
+                case 1:
+                    System.out.println("Go Sideways");
+                    break;
+
+                case 2:
+                    System.out.println("Jump");
+                    break;
+
+                case 3:
+                    System.out.println("Go Forward");
+                    break;
+
+                default:
+                    System.out.println("*** Repeat ***");
+            }
+
+            }
+        }
     }
-}
+
